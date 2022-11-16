106b737f5a83e18a4a4da68674fab66867d02882
Ruib.07 <ruibarreto07@outlook.com>
2022-11-16 15:56:56 +0000

    confirm

---
+++ MvcMusicStore/Models/MusicStoreEntities.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using System.Data.SqlServerCe;
+
+namespace MvcMusicStore.Models
+{
+    public class MusicStoreEntities : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        public System.Data.Entity.DbSet<MvcMusicStore.ViewModels.ShoppingCartViewModel> ShoppingCartViewModels { get; set; }
+    }
+}
---
+++ MvcMusicStore/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        [Key]
+        public string ShoppingCartId { get; set; }
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
